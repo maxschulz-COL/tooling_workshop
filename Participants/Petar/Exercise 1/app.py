1da287dee55b47ee9d38c7d70c5727fcb7436447
Maximilian Schulz <83698606+maxschulz-COL@users.noreply.github.com>
2025-03-26 10:23:24 +0000

    Initial commit

---
+++ Participants/Petar/Exercise 1/app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import plotly.express as px
+
+# Load the gapminder dataset
+df = px.data.gapminder()
+
+# Filter the dataset for the year 2007
+df_2007 = df[df["year"] == 2007]
+
+# Calculate the average life expectancy by continent
+avg_life_exp_by_continent = df_2007.groupby(
+    'continent')["lifeExp"].mean()
+
+
+# Print the result
+print(avg_life_exp_by_continent)
